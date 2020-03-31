88e9cc8a3e713528d83c14d8edefcd2dcee570cc
Yash Bhargava <yash.bhargava1992@gmail.com>
2020-03-31 13:14:35 +0530

    Test code to check if the newton method is working
    In the current version only one set of frequency is being used to get the radius for all three eqs.
    
    Future versions will determine a radius for all the frequencies and check if the same mass and spin works!

--- rpm_model_solving.py
+++ rpm_model_solving.py
@@ -1,7 +1,9 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import my_functions as mf
 
-freqs = np.loadtxt("freqs.txt",skiprows=1,unpack=True)
+
+freqs = np.loadtxt("../freqs.txt",skiprows=1,unpack=True)
 
 #print freqs[3]
 
@@ -9,7 +11,9 @@ for i in range(np.shape(freqs)[0]):
 	plt.plot(freqs[2],freqs[i],'.')
 plt.xscale('log')
 plt.yscale('log')
-plt.show()
+#plt.show()
+plt.clf()
+
 
 nu0 = freqs[0]
 nu1 = freqs[2]	# nu0,1,2,3 are sorted in freq
@@ -22,8 +26,35 @@ nu3 = freqs[3]
 
 ### Use these ranges once the code is finalised
 
-#mass_guess = np.arange(5,20,100)		# In solar masses
-#spin_guess = np.arange(-0.998,0.998,100) 	# The dimensionless spin paramter J/Mc2
+mass_guess = np.linspace(3.0,20.0,200)		# In solar masses
+spin_guess = np.linspace(0.,0.998,100) 		#For now only positive spins are considered. The dimensionless spin paramter J/Mc2
 
+#mass = 7
+#spin = 0.5 
 
+# The looping over the mass and spin shall be done using iterables. 
 
+##### Make a plot of all the freqs for a migven mass and spin to see which freq could correspond to which eq.
+
+r_guess = 10
+
+for mass in mass_guess:
+	for spin in spin_guess:
+		try:
+			r_orb = mf.newton_solver(r_guess,mf.nu_phi,nu2[0],mass,spin)
+		except RuntimeError:
+			#print "Runtime error"
+			r_orb = np.nan
+			#continue
+		try:
+			r_per = mf.newton_solver(r_guess,mf.nu_per,nu1[0],mass,spin)
+		except RuntimeError:
+			r_per = np.nan
+
+		try:
+			r_nod = mf.newton_solver(r_guess,mf.nu_nod,nu0[0],mass,spin)
+		except RuntimeError:
+			r_nod = np.nan
+
+		#print mass,spin,r_guess,r_orb,r_per,r_nod
+		if not(np.isnan(r_nod)) : print r_guess,r_orb,r_per,r_nod
