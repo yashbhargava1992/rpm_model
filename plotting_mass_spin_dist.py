b83d33d63975655822604cfcf765d2ee304aefee
Yash Bhargava <yash.bhargava1992@gmail.com>
2020-04-15 21:43:33 +0530

    Plotting code to plot the mass and spin dist
    The code read the files with data on mass and spin which can result into the obtained frequency
    It produces a normalised plot of the dsitibution for all observations combined together

---
+++ plotting_mass_spin_dist.py
@@ -0,0 +1,23 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import glob
+
+
+list_of_mass_spin_files = np.sort(glob.glob("mass_spin_sampling_20200413/*_mass_spin_flag.dat"))
+
+data_all = np.loadtxt(list_of_mass_spin_files[0])
+#data_all/=np.sum(data_all)
+for i,filename in enumerate(list_of_mass_spin_files):
+	if i==0: continue
+	data  =np.loadtxt(filename)
+	data_all+=data#/np.sum(data)
+
+data_all/=np.shape(list_of_mass_spin_files)[0]
+plt.imshow(data_all,origin='lower',extent=[0,0.998,2,20],aspect='auto')
+plt.xlabel("Spin")
+plt.ylabel("Mass")
+plt.colorbar()
+plt.savefig("mass_spin_distribution_overall_20200415.pdf")
+plt.show()
+		
+
